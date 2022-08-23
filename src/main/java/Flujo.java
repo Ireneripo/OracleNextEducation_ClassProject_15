48d6c3d94027d874d2fff44e4aeb5023826929da
Irene Riportella <riportellairene@gmail.com>
2022-08-23 18:59:26 +0200

    first commit

---
+++ src/main/java/Flujo.java
@@ -0,0 +1,36 @@
+public class Flujo {
+    public static void main(String[] args) {
+        System.out.println("Inicio del main");
+        metodo1();
+        System.out.println("Fin del main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Inicio del metodo1");
+        metodo2();
+        System.out.println("Fin del metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Inicio del metodo2");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+
+            try {
+                if(i == 3) {
+                    int num = 0;
+                    int resultado = i / num;
+                    System.out.println(resultado);
+                }
+
+                String test = null;
+                System.out.println(test.toString());
+            } catch (ArithmeticException | NullPointerException exception) {
+                System.out.println("Atrapó Excepción");
+                System.out.println(exception.getMessage());
+                exception.printStackTrace();
+            }
+        }
+        System.out.println("Fin del metodo2");
+    }
+}
